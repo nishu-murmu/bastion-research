9d5920f10ffaca8cbe672ccb3b614494dbfeec28
nishu-murmu <71123337+nishu-murmu@users.noreply.github.com>
2025-08-26 12:20:42 +0530

    chore: hidding my account

--- apps/web/src/components/generic/Header/DesktopNav.tsx
+++ apps/web/src/components/generic/Header/DesktopNav.tsx
@@ -84,8 +84,6 @@ const DesktopNav = ({ openSubmenu, setOpenSubmenu }) => {
         Login
       </Link>
 
-      <MyAccountDropdown />
-
       <Link
         to="/contact"
         className="bg-primary text-white px-6 py-2 rounded-lg hover:bg-red-700 transition-colors font-medium"
